43a27649e2f191d946df8f30c7a7e51ddd62d8df
Michal Superfine <superfin@g.jct.ac.il>
2024-08-28 16:44:01 +0300

    start of client

---
+++ pinterestClient/src/services/api.js
@@ -0,0 +1,29 @@
+// src/services/api.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api'; // Update to your server URL
+
+export const login = async (credentials) => {
+  const response = await axios.post(`${API_URL}/auth/login`, credentials);
+  return response.data;
+};
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/auth/register`, userData);
+  return response.data;
+};
+
+export const fetchFeed = async () => {
+  const response = await axios.get(`${API_URL}/media`);
+  return response.data;
+};
+
+export const uploadMedia = async (mediaData) => {
+  const response = await axios.post(`${API_URL}/media`, mediaData);
+  return response.data;
+};
+
+export const deleteMedia = async (mediaId) => {
+  const response = await axios.delete(`${API_URL}/media/${mediaId}`);
+  return response.data;
+};

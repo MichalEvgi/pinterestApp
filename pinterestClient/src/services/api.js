5319d02b96526c344f007101d94d56d658392f39
Michal Superfine <superfin@g.jct.ac.il>
2024-08-29 15:16:07 +0300

    api change

--- pinterestClient/src/services/api.js
+++ pinterestClient/src/services/api.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000/api'; 
 
 export const login = async (credentials) => {
-  const response = await axios.post(`${API_URL}/auth/login`, credentials);
+  const response = await axios.get(`${API_URL}/auth/login`, credentials);
   return response.data;
 };
 
 export const register = async (userData) => {
-  const response = await axios.post(`${API_URL}/auth/register`, userData);
+  const response = await axios.post(`${API_URL}/users`, userData);
   return response.data;
 };
 

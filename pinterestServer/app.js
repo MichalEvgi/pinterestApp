de841cf894b4f45387793ed4857fda3b1c03b779
michal <mevgi@g.jct.ac.il>
2024-09-03 19:46:15 +0300

    change

--- pinterestServer/app.js
+++ pinterestServer/app.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173' // Replace with your client-side URL
+  origin: 'http://localhost:5174' // Replace with your client-side URL
 }));
 
 app.use(logger('dev'));
